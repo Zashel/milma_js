5f5c4491d8f16e7fa56d0835b0cdc15e3c3a70e6
ivanuria <ivan.uria@gmail.com>
2020-06-02 12:12:22 +0200

    Determined the longest word. This is shitty

--- js-e4/js/main.js
+++ js-e4/js/main.js
@@ -5,3 +5,9 @@
 
 // Array to play with
 var valores = [true, 5, false, "hola", "adios", 2];
+
+// Determinar cuál de los dos elemenos de texto es mayor
+var valor_1 = valores[4];
+var valor_2 = valores[5];
+var valor_2_es_mayor = valor_2 > valor_1;
+alert("Adios es mas larga que hola");
