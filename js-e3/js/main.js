733fd106686f3ddffccf22ad0da6eb70e1c44984
ivanuria <ivan.uria@gmail.com>
2020-06-02 11:59:01 +0200

    Added variable meses and associated alert.

--- js-e3/js/main.js
+++ js-e3/js/main.js
@@ -2,3 +2,19 @@
 * En lugar de codigo.js porque para qué pornerle un nombre así si se puede seguir
 * una norma no "escribida".
 */
+
+// ¡¡¡Un array con los meses!!!
+var meses = ["Enero",
+             "Febrero",
+             "Marzo",
+             "Abril",
+             "Mayo",
+             "Junio",
+             "Julio",
+             "Agosto",
+             "Septiembre",
+             "Octubre",
+             "Noviembre",
+             "Diciembre"] // El mes doce se llama "mes diez", ¡pardiez!
+
+alert(meses);
