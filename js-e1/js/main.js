1f5a3b8fbe97966dabb9899d97e69069d068c364
ivanuria <ivan.uria@gmail.com>
2020-06-02 11:36:58 +0200

    Added second alert and comments on main.js

--- js-e1/js/main.js
+++ js-e1/js/main.js
@@ -1,1 +1,7 @@
-alert("Hola Mundo!");
+/* main.js
+* En lugar de codigo.js porque para qué pornerle un nombre así si se puede seguir
+* una norma no "escribida".
+*/
+
+alert("Hola Mundo!"); // Esto es una alerta que saluda al mundo. Un tropo en el sector.
+alert("Soy el primer script"); // Esto es otra alerta que se presenta. Aguardamos respuesta.
