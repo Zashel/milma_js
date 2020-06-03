ebe748ea97e31ffd0451c64ff9c3836e64313978
ivanuria <ivan.uria@gmail.com>
2020-06-03 12:08:39 +0200

    Added exercise to main.js

--- js-e5/js/main.js
+++ js-e5/js/main.js
@@ -2,3 +2,23 @@
 * En lugar de codigo.js porque para qué pornerle un nombre así si se puede seguir
 * una norma no "escribida".
 */
+
+/* Completar las condiciones de los if del siguiente script para que los
+* mensajes de los alert() se muestren siempre de forma correcta:
+*/
+
+var numero1 = 5;
+var numero2 = 8;
+
+if(...) {
+  alert("numero1 no es mayor que numero2");
+}
+if(...) {
+  alert("numero2 es positivo");
+}
+if(...) {
+  alert("numero1 es negativo o distinto de cero");
+}
+if(...) {
+  alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
