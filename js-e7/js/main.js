c33c9d5158afff4a0d9cae5e9fcb5524fc0db515
ivanuria <ivan.uria@gmail.com>
2020-06-03 12:44:09 +0200

    Resolved exercise

--- js-e7/js/main.js
+++ js-e7/js/main.js
@@ -10,3 +10,15 @@
 * Utilizando la estructura for, crear un script que calcule el factorial de un
 * número entero.
 */
+
+var numero = prompt("Deme un numerico entero, si a vuesa merced le place");
+
+if (numero%1 != 0) {
+  alert ("Por favor, no se porte mal con un servidor, que lo que me han de pedido que le de pida es un numero entero, sin decimales.");
+} else {
+  var factorial = 1;
+  for (var i = numero; i > 1; i--) { // tb var i = 1; i <= numero, i++
+    factorial = factorial*i;
+  }
+  alert(factorial);
+}
