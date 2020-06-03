59d4636243699666141f1b9ff0fd2768d8b29447
ivanuria <ivan.uria@gmail.com>
2020-06-03 12:11:51 +0200

    Finnished Exercise

--- js-e5/js/main.js
+++ js-e5/js/main.js
@@ -10,15 +10,15 @@
 var numero1 = 5;
 var numero2 = 8;
 
-if(...) {
+if(! numero1 > numero2) {
   alert("numero1 no es mayor que numero2");
 }
-if(...) {
+if(numero2 > 0) {
   alert("numero2 es positivo");
 }
-if(...) {
+if(numero1 < 0 || numero1 != 0) {
   alert("numero1 es negativo o distinto de cero");
 }
-if(...) {
+if(!((numero1+1)>=numero2)) {
   alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
 }
