cfdd4773bf3a3fd68676f81f9be559b5a175ec72
ivanuria <ivan.uria@gmail.com>
2020-06-03 17:48:02 +0200

    Exercise finished

--- js-e8/js/main.js
+++ js-e8/js/main.js
@@ -9,3 +9,24 @@
 * entero y devuelve como resultado una cadena de texto que indica si el número
 * es par o impar. Mostrar por pantalla el resultado devuelto por la función.
 */
+
+// Esta función devuelve si el número es par o impar.
+function es_par(numerin) {
+  // Comprobamos que el número sea real, porque si no, qué gracia
+  if (numerin%1 != 0) {
+    alert("Aaaamigo, que me des un numero real, ese no me vale.");
+    return; // Y salimos de la función, porque no nos vale.
+  } // podría poner un else, pero el return ya evita que se ejecute el resto
+
+  var que_es = "par"; // defino la variable con uno de los valores
+  if (numerin%2 == 1) { // compruebo si es impar, y si lo es, cambio el valor.
+    que_es = "impar";
+  }
+  alert("El numerin que m'as dao es " + que_es + ".");
+}
+
+// Y lo probamos porque somos unos fieras
+
+es_par(prompt("Ay mi arma, dame un numerin real que lo pueda cotejar..."));
+
+// Como puedes comprobar, soy fan de la programación funcional.
